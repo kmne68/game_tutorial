8bc9750d8d1ebbabcc804848185b4d2a55504bdd
Keith Emery <kmne68@gmail.com>
2018-07-06 14:21:12 -0500

    Add textures, tiles

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -11,6 +11,7 @@ import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
 
 import static helpers.Artist.*;
+import org.newdawn.slick.opengl.Texture;
 
 /**
  *
@@ -25,21 +26,29 @@ public class Boot {
 
         BeginSession();
         
-        float x = 100;
-        float y = 100;
-        float width = 50;
-        float height = 50;
+//        float x = 100;
+//        float y = 100;
+//        float width = 50;
+//        float height = 50;
     
+        Texture earth = loadTexture("res/earth.png", "PNG");
+        Texture grass = loadTexture("res/grass.png", "PNG");
+        Texture water = loadTexture("res/water.png", "PNG");
+        
         // Game loop
         while(!Display.isCloseRequested()) {
             
-            glBegin(GL_LINES);
-            glVertex2f(10, 10);
-            glVertex2f(100, 100);
-            glEnd();
+            DrawQuadTex(earth, 0, 0, 64, 64);
+            DrawQuadTex(grass, 0, 64, 64, 64);
+            DrawQuadTex(water, 0, 128, 64, 64);
+            
+//            glBegin(GL_LINES);
+//            glVertex2f(10, 10);
+//            glVertex2f(100, 100);
+//            glEnd();
 
-            drawQuad(50, 50, 100, 100);
-            drawQuad(150, 150, 100, 100);
+            //drawQuad(50, 50, 100, 100);
+            //drawQuad(150, 150, 100, 100);
             
             Display.update();
             Display.sync(60);
