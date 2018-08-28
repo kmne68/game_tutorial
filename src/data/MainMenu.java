e9d0fe310cb2bdf639b14e6960d67a419de2bd13
kmne68 <kmne68@gmail.com>
2018-08-28 15:03:03 -0500

    Add buttons and hook them into methods

--- src/data/MainMenu.java
+++ src/data/MainMenu.java
@@ -8,6 +8,9 @@ package data;
 import org.newdawn.slick.opengl.Texture;
 
 import static helpers.Artist.*;
+import helpers.StateManager;
+import static helpers.StateManager.gameState;
+import org.lwjgl.input.Mouse;
 import ui.UI;
 
 /**
@@ -23,8 +26,18 @@ public class MainMenu {
         
         background = quickLoad("mainmenu");
         menuUI = new UI();
-        menuUI.addButton("Play", "playButton", WIDTH / 2 - 128, (int)(HEIGHT * 0.45)); // last parameter places button 45% of the way down the screen
-
+        menuUI.addButton("Play", "playButton", WIDTH / 2 - 128, (int)(HEIGHT * 0.45f)); // last parameter places button 45% of the way down the screen
+        menuUI.addButton("Editor", "editorButton", WIDTH / 2 - 128, (int)(HEIGHT * 0.55f));
+        menuUI.addButton("Quit", "quitButton", WIDTH / 2 - 128, (int)(HEIGHT * 0.65f));
+    }
+    
+    
+    private void updateButtons() {
+        
+        if(Mouse.isButtonDown(0)) {
+            if(menuUI.isButtonClicked("Play"))
+                StateManager.setState(gameState.GAME);
+        }
     }
     
     
@@ -32,5 +45,6 @@ public class MainMenu {
         
         drawQuadTex(background, 0, 0, 2048, 1024);
         menuUI.draw();
+        updateButtons();
     }
 }
--- src/ui/UI.java
+++ src/ui/UI.java
@@ -5,9 +5,9 @@
  */
 package ui;
 
-import static helpers.Artist.drawQuadTex;
-import static helpers.Artist.quickLoad;
+import static helpers.Artist.*;
 import java.util.ArrayList;
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.opengl.Texture;
 
 /**
@@ -28,6 +28,30 @@ public class UI {
         
         buttonList.add(new Button(name, quickLoad(textureName), x, y));
         
+    }
+    
+    
+    public boolean isButtonClicked(String buttonName) {
+        
+        Button button = getButton(buttonName);
+        float mouseY = HEIGHT - Mouse.getY() - 1;
+        
+        if(Mouse.getX() > button.getX() && Mouse.getX() < button.getX() + button.getWidth() &&
+                mouseY > button.getY() && mouseY < button.getY() + button.getHeight()) {
+            return true;
+        }
+        return false;    
+    }
+    
+    
+    private Button getButton(String buttonName) {
+        
+        for(Button b : buttonList) {
+            if(b.getName().equals(buttonName)) {
+                return b;
+            }
+        }
+        return null;
     }
     
     
