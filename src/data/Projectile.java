2d98aab491552f565c1e63bb308aac9da257e0a5
Keith Emery <kemery@pnmg.com>
2018-09-07 19:16:40 -0500

    Add basic collision to projectiles

--- src/data/Projectile.java
+++ src/data/Projectile.java
@@ -9,68 +9,77 @@ import static helpers.Clock.delta;
 import static helpers.Artist.*;
 import org.newdawn.slick.opengl.Texture;
 
-
 /**
  *
  * @author Keith
  */
 public class Projectile {
-    
+
     private Texture texture;
     private float x, y;
+    private float width, height;
     private float xVelocity, yVelocity;     // projectile velocity
     private float speed;
     private int damage;
     private Enemy target;
-    
-    
-    public Projectile(Texture texture, Enemy target, float x, float y, float speed, int damage) {
-        
+    private boolean alive;
+
+    public Projectile(Texture texture, Enemy target, float x, float y, float width, float height, float speed, int damage) {
+
         this.texture = texture;
         this.target = target;
+        this.alive = true;
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
         this.speed = speed;
         this.damage = damage;
         this.xVelocity = 0f;
         this.yVelocity = 0f;
-        
+
         calculateDirection();
-        
+
     }
-    
-    
+
     private void calculateDirection() {
-        
+
         float totalAllowedMovement = 1.0f;
         // x and y are position of projectile
         float xDistanceFromTarget = Math.abs(target.getX() - x - (Game.TILE_SIZE / 4) + Game.TILE_SIZE / 2);
         float yDistanceFromTarget = Math.abs(target.getY() - y - (Game.TILE_SIZE / 4) + Game.TILE_SIZE / 2);
         float totalDistanceFromTarget = xDistanceFromTarget + yDistanceFromTarget;
         float xPercentOfMovement = xDistanceFromTarget / totalDistanceFromTarget;
-        
+
         xVelocity = xPercentOfMovement;
         yVelocity = totalAllowedMovement - xPercentOfMovement;
-        
-        if(target.getX() < x) {
+
+        if (target.getX() < x) {
             xVelocity *= -1;        // the target is to our tower's left so shoot in the negative x direction (left)
         }
-        if(target.getY() < y) {
+        if (target.getY() < y) {
             yVelocity *= -1;        // the target is above the tower so shoot in the negative y direction (up)
         }
     }
 
-    
     public void draw() {
-        
+
         drawQuadTex(texture, x, y, 32, 32);
     }
-    
-    
+
     public void update() {
-        
-        x += xVelocity * speed * delta();
-        y += yVelocity * speed * delta();
-        draw();
+
+        if (alive) {
+
+            x += xVelocity * speed * delta();
+            y += yVelocity * speed * delta();
+            if (checkCollision(x, y, width, height, target.getX(), target.getY(), target.getWidth(), target.getHeight())) {
+
+                // System.out.println("projectile hit the target");    
+                alive = false;
+            }
+
+            draw();
+        }
     }
 }
