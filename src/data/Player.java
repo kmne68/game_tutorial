76d118a5d430740205a4a77eeab61736a4a9f2d7
kmne68 <kmne68@gmail.com>
2018-09-12 14:09:12 -0500

    Add advanced targeting to cannons

--- src/data/Player.java
+++ src/data/Player.java
@@ -35,8 +35,7 @@ public class Player {
         this.index = 0;
         this.waveManager = waveManager;
         this.towerList = new ArrayList<TowerCannon>();
-        this.leftMouseButtonDown = false;
-        
+        this.leftMouseButtonDown = false;        
     }
     
     
@@ -44,12 +43,13 @@ public class Player {
         
         for(TowerCannon tc : towerList) {
             tc.update();
+            tc.updateEnemyList(waveManager.getCurrentWave().getEnemyList());
         }
         
         // handle Mouse input
         if (Mouse.isButtonDown(0) && !leftMouseButtonDown) {
             
-            towerList.add(new TowerCannon(quickLoad("cannonbase"), grid.getTile(Mouse.getX() / 64, (HEIGHT - Mouse.getY() - 1) / 64), 10, waveManager.getCurrentWave().getEnemyList()));
+            towerList.add(new TowerCannon(quickLoad("cannonbase"), grid.getTile(Mouse.getX() / 64, (HEIGHT - Mouse.getY() - 1) / 64), 10, 1000, waveManager.getCurrentWave().getEnemyList()));
             // TEST LINE: System.out.println("Mouse button 0 down.");
             // setTile();
         }
@@ -67,7 +67,7 @@ public class Player {
                 Clock.changeMultiplier(-0.2f);
             }
             if(Keyboard.getEventKey() == Keyboard.KEY_T && Keyboard.getEventKeyState()) {
-                towerList.add(new TowerCannon(quickLoad("cannonbase"), grid.getTile(6, 14), 10, waveManager.getCurrentWave().getEnemyList()));
+                towerList.add(new TowerCannon(quickLoad("cannonbase"), grid.getTile(6, 14), 10, 1000, waveManager.getCurrentWave().getEnemyList()));
             }
                 
         }
